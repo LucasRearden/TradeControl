00bbdfa4569c01aa2b772acd9caaf7fd76b9eee2
LucasRearden <lucasrearden2014@gmail.com>
2025-09-22 20:43:32 -0300

    setup ready

---
+++ frontend/src/TradeApp.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route, Navigate } from "react-router-dom"
+import { NavBar } from './components/NavBar'
+
+
+export const TradeApp = () => {
+
+
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default TradeApp
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../styles/navBar.css";
+import { NavLink } from 'react-router-dom';
+export const NavBar = () => {
+
+  return (
+    <>
+        <div className="nav-slim">
+            <div className="row">
+
+            </div>
+        </div>
+    </>
+  )
+}
